489136439b5f43d4e900854b7b9f3e97d645a63b
Kostas Niktas <kniktas@twilio.com>
2016-07-19 10:20:10 -0700

    Add the initial TwilML response endpoints

--- frinktwilioserver.py
+++ frinktwilioserver.py
@@ -1,20 +1,42 @@
 import frinkiaccommands
 import flask
+import twilio.twiml
+import cgi
 
 app = flask.Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    return "Hey there"
+    print str(flask.request.values)
+    return str(query(flask.request.values.get("Body"), tml=True))
 
 @app.route("/random")
 def random():
     return query("#random")
 
+@app.route("/t/random")
+def trandom():
+    return "<pre>{}</pre>".format(cgi.escape(query("#random", tml=True)))
+
+@app.route("/t/query/<querystr>")
+def tquery(querystr):
+    return "<pre>{}</pre>".format(cgi.escape(query(querystr, tml=True)))
+
 @app.route("/query/<querystr>")
-def query(querystr):
+def query(querystr, tml = False):
     response = frinkiaccommands.do_stuff(querystr)
+    if tml:
+        return _twiml(response)
     return _basic_html(response)
+
+def _twiml(frame):
+    response = twilio.twiml.Response()
+    if (frame["imageurl"] is not None):
+        with response.message(frame["message"]) as m:
+            m.media(frame["imageurl"])
+    else:
+        response.message(frame["message"])
+    return str(response)
 
 def _basic_html(frame):
     if not frame["error"]:
