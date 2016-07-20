68954c3ebccbe322301a45c0bc6dfd9f2c827cfa
Kostas Niktas <kniktas@twilio.com>
2016-07-20 11:16:27 -0700

    Rename the main server application

--- frinkiacsms.py
+++ frinkiacsms.py
@@ -1,4 +1,108 @@
-from frinktwilioserver import app as application
+import cgi
+from flask import Flask, request
+import frinkcommands
+import logging
+import os
+import twilio.twiml
+import sys
 
+app = Flask(__name__)
+
+logger = logging.getLogger('frinkiacsms')
+logger.setLevel(logging.DEBUG)
+fh = logging.handlers.RotatingFileHandler('frinkiacsms.log', maxBytes=100 * 1024**2)
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+@app.route("/smsinbound", methods=["GET", "POST"])
+def sms_inbound():
+    message_id = request.values.get("SmsMessageSid")
+    from_number = request.values.get("From")
+    message_body = request.values.get("Body")
+
+    logger.info(u"SMSReceived ID[{}] From[{}] Body[{}]".format(message_id, from_number, message_body))
+    twiml = _query(message_body, tml=True, mid=message_id)
+    return twiml
+
+@app.route("/")
+def hello():
+    return "It's Frinkiac SMS!"
+
+@app.route("/searchdebug", methods=["GET"])
+def search():
+    return """<html>
+    <head><title>Search Frinkiac SMS</title></head>
+    <body>
+        <form action="/searchdebug" method="POST">
+            <input type="text" size=50 name="Body"/><br/>
+            <label><input type="checkbox" value="TML" name="DebugTML"/>TwiML</label><br/>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>"""
+
+@app.route("/searchdebug", methods=["POST"])
+def search_result():
+    message_id = "WEBMESSAGE"
+    from_number = "THEINTERWEBZ"
+    message_body = request.values.get("Body")
+    tml_check = request.values.get("DebugTML")
+    logger.info(u"WebSMSReceived ID[{}] From[{}] Body[{}]".format(message_id, from_number, message_body))
+
+    if tml_check and tml_check == "TML":
+        search_response = u"<pre>{}</pre>".format(_query(message_body, tml=True, mid=message_id))
+    else:
+        search_response = _query(message_body)
+
+    return search_response
+
+@app.route("/random")
+def random():
+    return _query("#random")
+
+@app.route("/randomnocaption")
+def random_nocaption():
+    return _query("#random #nocaption")
+
+# Perform a query
+def _query(querystr, tml=False, mid=None):
+    frame = frinkcommands.do_stuff(querystr)
+    if tml:
+        response = _twiml(frame)
+    else:
+        response = _basic_html(frame)
+    logger.info(u"Query ID[{}] TML[{}] Reponse[{}]".format(mid, tml, response).encode('utf8'))
+    return response
+
+# Format a frame into a TwiML
+def _twiml(frame):
+    response = twilio.twiml.Response()
+    if (frame["imageurl"] is not None):
+        with response.message(frame["message"]) as m:
+            m.media(frame["imageurl"])
+    else:
+        response.message(frame["message"])
+    return str(response)
+
+# Format a frame with HTML
+def _basic_html(frame):
+    if not frame["error"]:
+            return """<html><body>
+<img src='{}'/><br/>
+<h3>{}</h3>
+</body></html>""".format(frame["imageurl"], frame["message"].replace("\n","</h3><h3>").encode('utf8'))
+    else:
+        return """<h3>There was an error or info:</h3><br/><bold><pre>{}</pre></bold>""".format(frame["message"])
+
+
+# RUN THE SERVER!!!
 if __name__ == "__main__":
-  application.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
